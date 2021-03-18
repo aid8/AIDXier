d7f6494da75628bdfadb5b2fcdbbcca9bc30602d
Aid <christiandia888@gmail.com>
2021-03-19 02:19:43 +0800

    Added Login and Signup with Firebase Auth
    Ok na may authentication na tas si uid nalang kinaag ko sa firestore kang user para kung gusto ta iaccess si ibang info niya (name, record, num, etc)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//Initialize
 const config = {
   apiKey: 'AIzaSyDbE43EtFgQ8K8H42uFBQLdG6903_d4hhw',
   authDomain: 'aidxier-19a98.firebaseapp.com',
@@ -8,115 +7,167 @@ const config = {
   appId: '1:385746246531:web:dcfb7e8d9ea77d056634b6',
 };
 
+//Initialize firebase
 firebase.initializeApp(config);
 const firestore = firebase.firestore();
 
-//Create Account Form for Patients And Doctors
 const signUpForm = document.querySelector('#signUpForm');
-const progressBar = document.querySelector('#progressBar');
-const progressHandler = document.querySelector('#progressHandler');
+const loginForm = document.querySelector('#loginForm')
+const logoutBtn = document.querySelector('#logoutBtn');
 const postSubmit = document.querySelector('#postSubmit');
+const progressBarForm = document.querySelector('#progressBarForm');
+const progressHandlerForm = document.querySelector('#progressHandlerForm');
 
 //Change Title and Keywords
 if (document.querySelector('#signUpTitle') != null) {
-  var id = window.location.href.split('?').pop();
+  let id = window.location.href.split('?').pop();
   let signUpTitle = document.querySelector('#signUpTitle');
-  var title = 'SIGN UP AS ';
+  let title = 'SIGN UP AS ';
   title += id == 'Patient' ? 'PATIENT' : 'MEDICAL PRACTICIONER';
   signUpTitle.textContent = title;
   if (id != 'Patient') {
-    document.querySelector('#recordInputText').textContent =
-      'Medical Practitioner Certificate';
+    document.querySelector('#recordInputText').textContent = 'Medical Practitioner Certificate';
   }
 }
 
+//USERS SIGN UP 
 if (signUpForm != null) {
-  let d;
-  var accountType = window.location.href.split('?').pop();
+  let d, uid;
+  const accountType = window.location.href.split('?').pop();
   signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault(); //Prevent refresh
-
-    //Validation
-    if (
-      document.getElementById('nameInput').value != '' &&
-      document.getElementById('emailInput').value != '' &&
-      document.getElementById('recordInput').files[0] != ''
-    ) {
-      let name = document.getElementById('nameInput').value;
-      let email = document.getElementById('emailInput').value;
-      let contact = document.getElementById('contactInput').value;
-      let record = document.getElementById('recordInput').files[0];
-      progressHandler.style.display = 'block';
-      console.log(record);
+    let name = document.getElementById('nameInput').value;
+    let email = document.getElementById('emailInput').value;
+    let pass = document.getElementById('passwordInput').value;
+    let contact = document.getElementById('contactInput').value;
+    let record = document.getElementById('recordInput').files[0];
+    let auth = firebase.auth();
+    let fieldsOk = true;
+    
+    //TODO : CHECK IF IT IS REAL EMAIL AND ALL FIELDS ARE OK
+    if(fieldsOk){
+      //Show Progess Bar
+      progressHandlerForm.style.display = 'block';
 
       const storageRef = firebase.storage().ref();
       const storageChild = storageRef.child(record.name);
 
-      console.log('Uploading file');
+      //Upload the file(record)
       const postCover = storageChild.put(record);
 
-      //Wait for the file to be Uploaded
+      //Wait for the file(record) to be Uploaded
       await new Promise((resolve) => {
-        postCover.on(
-          'stage_changed',
-          (snapshot) => {
-            let progress =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log(Math.trunc(progress));
-
-            if (progressHandler != null) {
-              progressHandler.style.display = true;
-            }
-
-            if (postSubmit != null) {
-              postSubmit.disabled = true;
-            }
-
-            if (progressBar != null) {
-              progressBar.value = progress;
-            }
-          },
-          (error) => {
-            //error log
-            //Add element to show error
-            console.log(error);
-          },
-          async () => {
-            const downloadURL = await storageChild.getDownloadURL();
-            d = downloadURL;
-            console.log(d);
-            //tell everything is done (exit await)
-            resolve();
-          }
-        );
+        postCover.on('stage_changed',(snapshot) => {
+          let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(Math.trunc(progress));
+          
+          progressHandlerForm.style.display = true;
+          progressBarForm.value = progress;
+          postSubmit.disabled = true;
+        },
+        (error) => {
+          //Add element to show error upon uploading
+          console.log(error);
+        },
+        async () => {
+          const downloadURL = await storageChild.getDownloadURL();
+          d = downloadURL;
+          console.log(d);
+          //tell everything is done (exit await)
+          resolve();
+        });
       });
-
+        
       const fileRef = await firebase.storage().refFromURL(d);
 
-      let post = {
-        name,
-        email,
-        contact,
-        accountType,
-        record: d,
-        fileref: fileRef.toString(),
-      };
+      //After uploading file (record), Create account in firebase auth and upload other info in storage
+      auth.createUserWithEmailAndPassword(email, pass)
+      .then(async(user) => {
+        uid = auth.currentUser.uid;
+        let accountInfo = {
+          uid,
+          name,
+          contact,
+          accountType,
+          record: d,
+          fileref: fileRef.toString(),
+        };
 
-      if (accountType == 'Patient') {
-        await firebase.firestore().collection('patient-accounts').add(post);
-      } else {
-        await firebase.firestore().collection('doc-accounts').add(post);
-      }
-      console.log('ACCOUNT CREATED');
+        if(accountType == 'Patient') {
+          await firebase.firestore().collection('patient-accounts-info').doc(uid).set(accountInfo);
+        } 
+        else{
+          await firebase.firestore().collection('doc-accounts-info').doc(uid).set(accountInfo);
+        }
 
-      if (postSubmit != null) {
-        window.location.replace('login.html?' + accountType);
+        //After a success sign up, go to homepage
+        if (postSubmit != null) {
+          gotoPage(accountType, "homepage");
+          postSubmit.disabled = false;
+        }
+      })
+      //if there are any errors in adding account (e.g. invalid email format)
+      .catch(function(ce){
+        console.log(ce.message);
         postSubmit.disabled = false;
-      }
-    } else {
+      });
+    }
+    else{
       //If there is a missing field or data
       //ADD a warning
       console.log('Please Input all fields!');
     }
   });
 }
+
+//USERS LOG IN
+if(loginForm != null){
+  loginForm.addEventListener('submit', async(e) =>{
+    e.preventDefault(); //Prevent refresh
+    let email = document.getElementById('emailInput').value;
+    let pass = document.getElementById('passwordInput').value;
+    let auth = firebase.auth();
+    
+    //Sign in
+    auth.signInWithEmailAndPassword(email, pass)
+    //if account not found
+    .catch(function(ce){
+      console.log(ce.message);
+    });
+
+    //get accounttype in firestore (change below)
+    const accountType = window.location.href.split('?').pop();
+    gotoPage(accountType, "homepage");
+  });
+}
+
+function gotoPage(accountType, page){
+  let homepageURL = (accountType == 'Patient') ? "patient/" : "medical-practitioner/";
+  switch(page){
+    case "homepage":
+      window.location.replace(homepageURL+page+".html");
+    break;
+
+    case "test":
+
+    break;
+
+    default:
+  }
+}
+
+if(logoutBtn != null){
+  //firebase.auth().signOut();
+}
+
+//A realtime listener firebase
+firebase.auth().onAuthStateChanged(firebaseUser =>{
+  if(firebaseUser){
+    console.log("logged in" + firebaseUser);
+    //logoutBtn.classList.remove('hide');
+  }
+  else{
+    console.log("not logged in");
+    //logoutBtn.classList.add('hide');
+  }
+});
