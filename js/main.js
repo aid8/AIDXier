1fa4156c57581cc8bfb81a33772d14661dfe551e
Aid <christiandia888@gmail.com>
2021-03-22 19:55:32 +0800

    Updated set-appointment and added browse-clinics
    = Set-Appointment has now google calendar api, it will connect if you load the project in a localhost/live server with port 5500 (e.g localhost:5500/index). But still it cannot create events in google calender, it can only show users events.
    
    = Can choose clinics in setting appointment, can set appointment with pending page, and cancel appointment.
    
    = Browse-clinics will list all doctors and their clinics.
    
    = Updated sessions storage to be in one array.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 /*TODO:
   = CHECK ALL FIELDS IN SIGN UP IS FILLED UP AND ADD NOTIFICATION TO USER
   = ADD NOTIFICATION IN LOGIN
-  = ADD GOOGLE CALENDAR API
+  = IN SETTING APPOINTMENTS, ADD AS WELL TO GOOGLE CALENDAR, CHECK IF DATE AND TIME WILL CONFLICT
   = ADD GOOGLE MAP API
+  = DO NOT DIRECTLY GO TO LOGIN.HTML, must have ?patient or ?doc
+  = IF LOGGED IN, DO NOT GO TO LOGIN.HTML nor signup, vice versa
 */
 
 const config = {
@@ -29,6 +31,10 @@ firebase.initializeApp(config);
 const firestore = firebase.firestore();
 
 //----------GLOBAL VARIABLES----------//
+/*
+  May slowdown page due to many global variabes
+  Some variables can be local variables
+*/
 const signUpForm = document.querySelector('#signUpForm');
 const loginForm = document.querySelector('#loginForm')
 const logoutBtn = document.querySelector('#logoutBtn');
@@ -40,6 +46,14 @@ const titleSignUp = document.querySelector('#titleSignUp');
 const userNameHomepage = document.querySelector('#userNameHomepage');
 const userInfoHomepage = document.querySelector('#userInfoHomepage');
 const patientRecordPage = document.querySelector('#patientRecord');
+const setAppointmentDiv = document.querySelector('#setAppointmentDiv');
+const googleCalSignInBtn = document.querySelector('#googleCalSignInBtn');
+const googleCalSignOutBtn = document.querySelector('#googleCalSignOutBtn');
+const googleCalSignInDiv = document.querySelector('#googleCalSignInDiv');
+const googleCalEventDiv = document.querySelector('#googleCalEventDiv');
+const setAppointmentSubmitBtn = document.querySelector('#setAppointmentSubmitBtn');
+const pendingAppointmentDiv = document.querySelector('#pendingAppointmentDiv');
+const browseClinicsDiv = document.querySelector('#browseClinicsDiv');
 
 //----------USERS SIGN UP----------//
 if (signUpForm != null) {
@@ -116,6 +130,8 @@ if (signUpForm != null) {
           contact,
           accountType,
           record: d,
+          appointments: [],
+          prevAppointments: [],
           fileref: fileRef.toString(),
         };
 
@@ -130,6 +146,7 @@ if (signUpForm != null) {
 
         //After a success sign up, go to homepage
         if (postSubmit != null) {
+          storeData(accountInfo);
           gotoPage("homepage", accountType);
           postSubmit.disabled = false;
         }
@@ -172,6 +189,7 @@ if(loginForm != null){
           }
         });
       });
+      
       storeData(data);
       gotoPage("homepage", getUserType());
     })
@@ -221,18 +239,14 @@ function gotoPage(page, accountType){
 }
 
 //Only showing the current link
+//Fix if needed
 function getCurrentPage(){
   let loc = window.location.href;
   console.log(loc);
 }
 
-function userLoggedIn(){
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      return true;
-    }
-  });
-  return false;
+function reloadPage(){
+  location.reload();
 }
 
 function getUserType(){
@@ -241,62 +255,258 @@ function getUserType(){
   if(accountType == "Patient" || accountType == "Doc"){
     return accountType;
   }
-  return sessionStorage.getItem("accountType");
+  return getUserData().accountType;
 }
 
 //store data in storageSession for faster loading time
 function storeData(accountInfo){
-  sessionStorage.setItem("name", accountInfo.name);
-  sessionStorage.setItem("age", accountInfo.age);
-  sessionStorage.setItem("gender", accountInfo.gender);
-  sessionStorage.setItem("contact", accountInfo.contact);
-  sessionStorage.setItem("accountType", accountInfo.accountType);
-  sessionStorage.setItem("record", accountInfo.record);
-  if(accountInfo.accountType == "Doc"){
-    sessionStorage.setItem("clinicName", accountInfo.clinicName);
-    sessionStorage.setItem("clinicAddress", accountInfo.clinicAddress);
-  }
+  sessionStorage.setItem("accountInfo",JSON.stringify(accountInfo));
 }
 
 function getUserData(){
-  let accountInfo = {};
-  accountInfo.name = sessionStorage.getItem("name");
-  accountInfo.age = sessionStorage.getItem("age");
-  accountInfo.gender = sessionStorage.getItem("gender");
-  accountInfo.contact = sessionStorage.getItem("contact");
-  accountInfo.accountType = sessionStorage.getItem("accountType");
-  accountInfo.record = sessionStorage.getItem("record");
-  if(accountInfo.accountType == "Doc"){
-    accountInfo.clinicName = sessionStorage.getItem("clinicName");
-    accountInfo.clinicAddress = sessionStorage.getItem("clinicAddress");
-  }
-  return accountInfo;
+  return JSON.parse(sessionStorage.getItem("accountInfo"));
 }
 
-async function changePageDetails(){
-  if(userLoggedIn){
-    let userData = await getUserData();
-    let accountType = await getUserType();
-    //Homepage
-    if(userNameHomepage != null){
-      userNameHomepage.textContent = userData.name;
-
-      if(accountType == "Patient"){
-        userInfoHomepage.textContent = userData.age + ", " + userData.gender;
+async function reloadData(){
+  let collection;
+  let data = await new Promise(resolve =>{
+    firebase.auth().onAuthStateChanged(async(user) =>{
+      if(user){
+        collection = (await getUserType() == "Patient") ? "patient" : "doc";
+        firestore.collection(collection+"-accounts-info").doc(user.uid).get().then((doc)=>{
+          if(doc.exists){
+            resolve(doc.data());
+          }
+        });
       }
-      else{
-        userInfoHomepage.textContent = userData.clinicName + ", " + userData.clinicAddress;
+    });
+  });
+  storeData(data);
+}
+
+async function getClinics(){
+  let clinics = new Set();
+  let docs = await firestore.collection("doc-accounts-info").get();
+  docs.forEach(doc =>{
+    clinics.add(doc.data().clinicName);
+  });
+  return clinics;
+}
+
+async function getDoctorInfos(){
+  let doctors = [];
+  let docs = await firestore.collection("doc-accounts-info").get();
+  docs.forEach(doc =>{
+    doctors.push(doc.data());
+  });
+  return doctors;
+}
+
+async function addAppointment(date, symptom, clinicSelected){
+  let userData = getUserData();
+  let dataRef = await firebase.firestore().collection('patient-accounts-info').doc(userData.uid);
+  let currentAppointments = userData.appointments;
+  currentAppointments.push(JSON.parse(JSON.stringify({
+    date,
+    symptom,
+    clinicSelected,
+    viewed: false,
+    approved: false,
+    patientUid: userData.uid,
+    details:""
+  })));
+  dataRef.update({appointments: currentAppointments});
+  await reloadData();
+  reloadPage();
+}
+
+//Soon add functions that will be able to cancel multiple appointments
+async function cancelAppointment(){
+  let dataRef = await firebase.firestore().collection('patient-accounts-info').doc(getUserData().uid);
+  dataRef.update({appointments: []});
+  await reloadData();
+  reloadPage();
+}
+
+function changePageDetails(){
+  firebase.auth().onAuthStateChanged(async(user) => {
+    if(user){
+      let userData = await getUserData();
+      let accountType = await getUserType();
+      //=====Homepage=====//
+      if(userNameHomepage != null){
+        userNameHomepage.textContent = userData.name;
+        if(accountType == "Patient"){
+          userInfoHomepage.textContent = String(userData.age) + ", " + userData.gender;
+        }
+        else{
+          userInfoHomepage.textContent = userData.clinicName + ", " + userData.clinicAddress;
+        }
+      }
+
+      //=====Medical Records=====//
+      if(patientRecordPage != null){
+        patientRecordPage.style.backgroundRepeat = "no-repeat";
+        patientRecordPage.style.backgroundSize = "100% 100%";
+        patientRecordPage.style.backgroundImage = "url(" + userData.record + ")";
+      }
+
+      //=====Set-Appointment=====//
+      if(userData){
+        if(userData.appointments.length <= 0){
+          if(setAppointmentDiv != null) setAppointmentDiv.style.display = 'block';
+          if(pendingAppointmentDiv != null) pendingAppointmentDiv.style.display = 'none';
+        }
+        else{
+          if(setAppointmentDiv != null) setAppointmentDiv.style.display = 'none';
+          if(pendingAppointmentDiv != null) pendingAppointmentDiv.style.display = 'block';
+        }
+      }
+
+      if(setAppointmentDiv != null){
+        if(setAppointmentDiv.style.display != 'none'){
+          let clinicChooseSelect = document.getElementById('clinicChooseSelect');
+          let date = document.getElementById('dateInput');
+          let symptom = document.getElementById('symptomInput');
+          let clinics = await getClinics();
+          clinics.forEach(clinic =>{
+            var option = document.createElement("option");
+            option.text = clinic;
+            option.value = clinic;
+            clinicChooseSelect.add(option);
+          });
+
+          googleCalSignOutBtn.style.display = 'none';
+          googleCalEventDiv.style.display = 'none';
+          handleClientLoad();
+    
+          setAppointmentSubmitBtn.addEventListener('click', async(e) =>{
+            e.preventDefault();
+            let selectOption = clinicChooseSelect.options[clinicChooseSelect.selectedIndex];
+            let clinicSelected = selectOption.getAttribute("value");
+            await addAppointment(date.value,symptom.value,clinicSelected);
+          });
+        }
+      }
+
+      if(pendingAppointmentDiv != null){
+        if(pendingAppointmentDiv.style.display != 'none'){
+          reloadData();
+          let pendingStatusTitle = document.getElementById('pendingStatusTitle');
+          let clinicDetailsPendingTitle = document.getElementById('clinicDetailsPendingTitle');
+          let appointmentSchedulePendingPar = document.getElementById('appointmentSchedulePendingPar');
+          let appointmentSymptomPendingPar = document.getElementById('appointmentSymptomPendingPar');
+          let appointmentPendingBtn = document.getElementById('appointmentPendingBtn');
+          let responseDetailsPar = document.getElementById('responseDetailsPar');
+
+          clinicDetailsPendingTitle.innerHTML = userData.appointments[0].clinicSelected;
+          appointmentSchedulePendingPar.innerHTML = "Schedule: " + userData.appointments[0].date;
+          appointmentSymptomPendingPar.innerHTML = "Symptom: " + userData.appointments[0].symptom;
+          if(userData.appointments[0].viewed){
+            let approved = userData.appointments[0].approved;
+            pendingStatusTitle.innerHTML = "Response Recived: " + (approved ? 'Approved' : 'Rejected');
+            responseDetailsPar.innerHTML = "Details: " + userData.appointments[0].details;
+            appointmentPendingBtn.innerHTML = (approved ? '' : 'Close');
+            appointmentPendingBtn.style.display = (approved ? 'none' : 'block');
+          }
+          else{
+            pendingStatusTitle.innerHTML = 'Pending response from';
+            appointmentPendingBtn.style.display = 'block';
+            appointmentPendingBtn.innerHTML = 'Cancel Appointment';
+          }
+          appointmentPendingBtn.addEventListener('click', e=>{cancelAppointment();});
+        }
+      }
+
+      //=====Browse Clinic=====//
+      if(browseClinicsDiv != null){
+        var doctors = await getDoctorInfos();
+        doctors.forEach(function(doctor, i){
+          var divCard = document.createElement('div');
+          var divCardBody = document.createElement('div');
+          var pCardBody = document.createElement('p');
+          pCardBody.innerHTML = doctor.name + " / " + doctor.clinicName;
+          divCard.className = "card";
+          divCardBody.className = "card-body";
+          pCardBody.className = "card-text";
+
+          divCardBody.appendChild(pCardBody);
+          divCardBody.insertAdjacentHTML('beforeEnd','<a class="card-link" data-bs-toggle="modal" data-bs-target="#clinicDetailModal" data-bs-index="' + i +'">More Details</a>');
+          divCard.appendChild(divCardBody);
+          browseClinicsDiv.appendChild(divCard);
+        });
+
+        var clinicDetailModal = document.getElementById('clinicDetailModal')
+        clinicDetailModal.addEventListener('show.bs.modal', function (event) {
+          console.log("?");
+          // Button that triggered the modal
+          var button = event.relatedTarget
+          // Extract info from data-bs-* attributes
+          var index = button.getAttribute('data-bs-index');
+          var selectedDoctor = doctors[index];
+          var modalTitle = clinicDetailModal.querySelector('.modal-title')
+          var modalBodyInput = clinicDetailModal.querySelector('.modal-body')
+
+          modalTitle.textContent = selectedDoctor.clinicName;
+          modalBodyInput.innerHTML = "Doctor: " + selectedDoctor.name + "<br/>Address: " + selectedDoctor.clinicAddress;
+        });
       }
     }
+  });
+}
 
-    //MedicalRecordsPage
-    if(patientRecordPage != null){
-      patientRecordPage.style.backgroundRepeat = "no-repeat";
-      patientRecordPage.style.backgroundSize = "100% 100%";
-      patientRecordPage.style.backgroundImage = "url(" + userData.record + ")";
-      console.log( "url(" + userData.record + ")");
-    }
+//=====GOOGLE CALENDAR FUNCTIONS====//
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+  gapi.client.init({
+    apiKey: googleCalConfig.API_KEY,
+    clientId: googleCalConfig.CLIENT_ID,
+    discoveryDocs: googleCalConfig. DISCOVERY_DOCS,
+    scope: googleCalConfig.SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+   
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    googleCalSignInBtn.onclick = handleAuthClick;
+    googleCalSignOutBtn.onclick = handleSignoutClick;
+  }, function(error) {
+    console.log(JSON.stringify(error, null, 2));
+  });
+}
+
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    googleCalSignInDiv.style.display = 'none';
+    googleCalSignOutBtn.style.display = 'block';
+    googleCalEventDiv.style.display = 'block';
+    showCalendar();
+  } else {
+    googleCalEventDiv.style.display = 'none';
+    googleCalSignOutBtn.style.display = 'none';
+    googleCalSignInDiv.style.display = 'block';
   }
+}
+
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+  reloadPage();
+}
+
+function showCalendar() {
+  let profile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
+  let userEmail = profile.getEmail();
+  let calendarIframe = '<iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=' + userEmail + '&amp;color=%232952A3&amp; style=" border-width:0 " width="100%" height="400" frameborder="0" scrolling="no"></iframe>';
+  googleCalEventDiv.style.display = 'block';
+  googleCalEventDiv.innerHTML = calendarIframe;
 }
 
 //----------FIREBASE REALTIME LISTENER----------//
